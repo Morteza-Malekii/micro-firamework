2fe3b24e48df345fba3e57f83332dda51e9a2f97
morteza <mortezaghube@gmail.com>
2025-03-23 14:38:20 +0300

    front controler and composer and autoloading is ok

---
+++ App/Core/test.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Core;
+
+class test
+{
+    public function __construct()
+    {
+        $text =  "test is ok";
+        return $text;
+    }
+}
---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+# Front contoller
+
+use App\Core\test;
+
+include "vendor/autoload.php";
+
+// echo $_SERVER['REQUEST_URI'];
+
+new test;
